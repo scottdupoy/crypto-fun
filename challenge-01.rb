f542afa54edb0b777eebd4ce07c15cb2e35fe082
Scott Dupoy <sdupoy@aurielequities.co.uk>
2015-02-09 11:26:27 +0000

    encoder now can handle odd numbers of hex characters and will use trailing '=' characters in base 64 encoding when the number of bytes the hex generates is not divisible by 3

--- challenge-01.rb
+++ challenge-01.rb
@@ -12,12 +12,13 @@ def ConvertHexCharToInt(hexChar)
 end
 
 def ConvertHexStringToBytes(hexString)
-    # ensure that the hex matches byte boundaries
-    if hexString.length % 2 != 0
-        raise "hex input string expected to be a multiple of 2 so complete bytes"
+    # if the hex has an odd number of characters then prefix with a '0' so that
+    # the byte boundaries work out correctly
+    if hexString.length % 2 == 1
+        hexString = "0" + hexString
     end
-   
-    # two hex characters => 1 byte
+
+    # two hex characters => 1 byte. add one just in case we 
     byteCount = hexString.length / 2
     bytes = Array.new(byteCount)
 
@@ -60,25 +61,57 @@ def ConvertSixBitNumberToBase64Char(sixBitNumber)
     raise "Cannot convert number > 63 to base 64 char"
 end
 
+def ConvertBytesToInt(bytes)
+    # work back from least significant byte
+    value = 0
+    lastByte = bytes.length - 1
+    for i in 0..lastByte
+        index = lastByte - i
+        byte = bytes[index]
+        value += byte << (i * 8)
+    end
+    return value
+end
+
 def ConvertHexToBase64(hexString)
     bytes = ConvertHexStringToBytes(hexString)
     
-    # assert that it's in an a multiple of 3 for now
-    if bytes.length % 3 != 0
-        raise "Can't exactly represent the hex in base 64"
-    end
+    # # assert that it's in an a multiple of 3 for now
+    # if bytes.length % 3 != 0
+        # raise "Can't exactly represent the hex in base 64"
+    # end
     
     # walk along the bytes in blocks of 3    
     result = ""
     index = 0
     while index < bytes.length
-        triple = bytes[index] << 16 | bytes[index + 1] << 8 | bytes[index + 2]
+        # pull out the bytes to use
+        byte0 = bytes[index];        
+        byte1 = (index + 1) < bytes.length ? bytes[index + 1] : 0;
+        byte2 = (index + 2) < bytes.length ? bytes[index + 2] : 0;
+        
+        # join the bytes
+        triple = byte0 << 16 | byte1 << 8 | byte2
+        
+        # walk along the bytes and build the string
         for tripleIndex in 0..3
-            # need to start from left so really it's 3 => 0
+            # take characters from left so really it's 3 => 0
             shift = 3 - tripleIndex;
+            
+            # if we have reached the end of the string then last couple of characters may be '='
+            # have made the following more concise, but leave fuller version as a comment as it's clearer what's going on:
+            #   if (shift == 1 && (index + 1) >= bytes.length) ||  (shift == 0 && (index + 2) >= bytes.length)
+            if shift < 2 && (index + (2 - shift)) >= bytes.length
+                result += "="
+                next
+            end
+            
+            # not at the end of the string, so take the correct 6 bits and convert them to base 64
             sixBitSection = (triple >> shift * 6) & 0x3f # mask out 2 msbs: 00111111
             result += ConvertSixBitNumberToBase64Char(sixBitSection)
         end
+                
+        # move to the next set of bytes
         index += 3
     end 
     return result
@@ -89,3 +122,4 @@ base64String = ConvertHexToBase64(hexString);
 
 puts "hex:    " + hexString
 puts "base64: " + base64String
+

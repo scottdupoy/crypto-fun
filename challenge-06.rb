41fe02b7ac61713254d28b1d44d43a5a86423bd8
Scott Dupoy <scott@dupoy.com>
2015-02-10 21:37:33 +0000

    hamming distance calculations working correctly

---
+++ challenge-06.rb
@@ -0,0 +1,36 @@
+require "./crypto_utils.rb"
+
+def sumBits(byte)
+    sum = 0
+    for b in 0..7
+        sum += (((1 << b) & byte)) > 0 ? 1 : 0
+    end
+    sum
+end
+
+def calculateDistance(string1, string2)
+    puts "string1: " + string1
+    puts "string2: " + string2
+
+    if string1.length != string2.length
+        raise "can't calculate distance between 2 different length strings"
+    end
+
+    if string1 == string2
+        return 0
+    end
+
+    distance = 0
+    for index in 0..(string1.length - 1)
+        byte1 = string1[index].ord
+        byte2 = string2[index].ord
+        distance += sumBits(byte1 ^ byte2)
+    end
+    distance
+end
+
+a = "this is a test"
+b = "wokka wokka!!!"
+
+distance = calculateDistance(a, b)
+puts "distance: " + distance.to_s()
